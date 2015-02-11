913ee79436a926d312dcdd66944b4133c81a6b10
Tony Wu <tony.wu@berkeley.edu>
2015-02-10 16:33:06 -0800

    changed invocation code

--- week3/week3.lua
+++ week3/week3.lua
@@ -23,10 +23,10 @@ csock = storm.net.udpsocket(announcement_port,
 		print("Got response: %s", payload)
                 local unpacked = storm.mp.unpack(payload)
 		-- if we receive an announcement
-		if port == announcement_port then
+		--if port == announcement_port then
 			local id = unpacked["id"]
 			for k,v in pairs(unpacked) do 
-				print("service: key: value: ", k,v)
+				--print("service: key: value: ", k,v)
 				if k ~= "id" then
 					-- If not id, map the id and address to the services
 					if services[k] then
@@ -39,15 +39,18 @@ csock = storm.net.udpsocket(announcement_port,
 						services[k] = t
 					end
 				end
-				for ke, va in pairs(services) do
+				--[[for ke, va in pairs(services) do
 					print("saved: ", ke, va)
-				end
+				end]]--
 			end
 		-- if we receive a service invocation
-		elseif port == invocation_port then
+		--[[elseif port == invocation_port then
 			-- get the service name
+			print ("invoked 1")
 			local s_name = unpacked[1]
+			print("service: ", s_name)
 			if s_name == "printString" then
+				print("inside print stirng")
 				rtn = svc_stdout(from, port, unpacked[2][1])
 				sendMessage(rtn, port, from)
 			elseif s_name == "getNow" then
@@ -56,9 +59,26 @@ csock = storm.net.udpsocket(announcement_port,
 			else
 				print("Unsupported service")
 			end	
-		end
+		end]]--
 	end)
 
+isock = storm.net.udpsocket(invocation_port,
+	function(payload, from, port)
+		-- get the service name
+			print ("invoked 1")
+			local s_name = unpacked[1]
+			print("service: ", s_name)
+			if s_name == "printString" then
+				print("inside print stirng")
+				rtn = svc_stdout(from, port, unpacked[2][1])
+				sendMessage(rtn, port, from)
+			elseif s_name == "getNow" then
+				rtn = svc_getNow()
+				sendMessage(rtn, port, from)
+			else
+				print("Unsupported service")
+			end
+	end)
 
 -- get ip and addresses associated with a service
 function getMapping(service)
@@ -99,6 +119,10 @@ function sendMessage(msg, port, address)
 	
 end
 
+function sendInvokeMessage(msg, port, address)
+	storm.net.sendto(isock, storm.mp.pack(msg), address, port)
+end
+
 -- periodically send out our services
 if sendHandle == nil then
 	sendHandle = storm.os.invokePeriodically(2000*storm.os.MILLISECOND, sendMessage, announcement, announcement_port, "ff02::1")
@@ -107,7 +131,7 @@ end
 -- service invocation function
 function invokeFunction(name, params) 
 	local msg = {name, params}
-	sendMessage(storm.mp.pack(msg), invocation_port, "ff02::1")
+	sendInvokeMessage(storm.mp.pack(msg), invocation_port, "ff02::1")
 end
 
 sh.start()
